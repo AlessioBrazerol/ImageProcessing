d937d53cddf959858899972c551b7537fb237566
apple00juice <alessio999@hotmail.ch>
2020-05-01 12:41:38 +0200

    Finished function for convolution Created simple examples: - spatialLowpass - spatialHighpass - sharpen - gaussianBlur3x3 - gaussianBlur5x5

--- src/convolution.py
+++ src/convolution.py
@@ -1,11 +1,61 @@
 from PIL import Image
-
+from progress.bar import Bar
 
 def convolution(input_image, structuring_element):
+    # Create Progress Bar
+    bar = Bar('Creating Image', max=input_image.size[0]*input_image.size[1])
+
     # Create the output image and load it
     output_image = Image.new("RGB", input_image.size, "white")
     image_map = output_image.load()
 
+    # Get the structuring matrix and center of structuring element and normalize
+    structuring_matrix = structuring_element[0]
+    se_center = structuring_element[1]
+    normalize = structuring_element[2]
+
+    # Look at all pixel in the original Image
+    for y in range(0, input_image.size[1]):
+        for x in range(0, input_image.size[0]):
+
+            color_new = (0, 0, 0)
+            # Normalize amount, can change because of edge pixel
+            n = 0
+
+            # Go through all positions in the structuring_matrix
+            for row_idx, row in enumerate(structuring_matrix):
+                for column_idx in range(0, len(row)):
+                    # print("({},{}) = {}".format(column_idx - se_center[0], row_idx - se_center[1], row[column_idx]))
+
+                    # Calculate coordinates of pixels to check for structuring element
+                    pixel_x = x + column_idx - se_center[0]
+                    pixel_y = y + row_idx - se_center[1]
 
 
+                    # Check if pixel_x and pixel_y are inside the image
+                    if 0 <= pixel_x < input_image.size[0] and 0 <= pixel_y < input_image.size[1]:
+                        # Create new color out of neighbour colors
+                        factor = row[column_idx]
+
+                        pixel_color = input_image.getpixel((pixel_x, pixel_y))
+                        n += factor
+
+                        # Check if gray-scale image
+                        if isinstance(pixel_color, int):
+                            color_temp = color_new[0] + factor * pixel_color
+                            color_new = (color_temp, color_temp, color_temp)
+
+                        else:
+                            color_r = color_new[0] + factor * pixel_color[0]
+                            color_g = color_new[1] + factor * pixel_color[1]
+                            color_b = color_new[2] + factor * pixel_color[2]
+                            color_new = (color_r, color_g, color_b)
+
+            if normalize:
+                print(n)
+                color_new = (color_new[0] // n, color_new[1] // n, color_new[2] // n)
+
+            image_map[x, y] = color_new
+            bar.next()
+    bar.finish()
     return output_image
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from PIL import Image
+from math import cos,sin
 from src.binaryimageprocessing import dilate_binary_image, erode_binary_image
+from src.convolution import  convolution
 
 outputPath = "C:/Users/aless/Desktop/image.png"
 binaryImagePath = "C:/Users/aless/Desktop/original.png"
@@ -12,6 +14,57 @@ structuringElement3 = {(0, 5)}
 image1 = dilate_binary_image(binaryImage, structuringElement)
 image2 = erode_binary_image(image1, structuringElement)
 
-image1.show()
-image2.show()
+# image1.show()
+# image2.show()
 # image.save(outputPath);
+
+lennaImagePath = "C:/Users/aless/Desktop/image2.jpg"
+lennaImage = Image.open(lennaImagePath)
+
+spatiaLowpassMatrix = [[1, 1, 1],
+                       [1, 1, 1],
+                       [1, 1, 1]]
+
+spatiaLowpass = (spatiaLowpassMatrix, (1, 1), True)
+
+spatialHighpassMatrix = [[0, -1, 0],
+                         [-1, 4, -1],
+                         [0, -1, 0]]
+
+spatialHighpass = (spatialHighpassMatrix, (1, 1), False)
+
+sharpenMatrix = [[0, -1, 0],
+                 [-1, 5, -1],
+                 [0, -1, 0]]
+
+sharpen = (sharpenMatrix, (1, 1), False)
+
+gaussianBlur3x3Matrix = [[1, 2, 1],
+                         [2, 4, 2],
+                         [1, 2, 1]]
+
+gaussianBlur3x3 = (gaussianBlur3x3Matrix, (1, 1), True)
+
+gaussianBlur5x5Matrix = [[1, 4, 6, 4, 1],
+                         [4, 16, 24, 16, 4],
+                         [6, 24, 36, 24, 6],
+                         [4, 16, 24, 16, 4],
+                         [1, 4, 6, 4, 1]]
+
+gaussianBlur5x5 = (gaussianBlur5x5Matrix, (1, 1), True)
+
+lenaCopy = convolution(lennaImage, spatiaLowpass)
+lenaCopy.show("spatiaLowpass")
+
+lenaCopy = convolution(lennaImage, spatialHighpass)
+lenaCopy.show("spatialHighpass")
+
+lenaCopy = convolution(lennaImage, sharpen)
+lenaCopy.show("sharpen")
+
+lenaCopy = convolution(lennaImage, gaussianBlur3x3)
+lenaCopy.show("gaussianBlur3x3")
+
+lenaCopy = convolution(lennaImage, gaussianBlur5x5)
+lenaCopy.show("gaussianBlur5x5")
+
