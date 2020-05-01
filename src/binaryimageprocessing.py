37f08e79b94c47bdcc3458da41a741e9ccba09dd
apple00juice <alessio.bra@hotmail.ch>
2020-05-01 10:05:39 +0200

    Added some comments

--- src/binaryimageprocessing.py
+++ src/binaryimageprocessing.py
@@ -8,8 +8,11 @@ with A = all black pixels
 
 
 def dilate_binary_image(input_image, structuring_element):
+    # Create the output image and load it
     output_image = Image.new("RGB", input_image.size, "white")
     image_map = output_image.load()
+    
+    # Look at all pixel in the original Image
     for y in range(0, input_image.size[1]):
         for x in range(0, input_image.size[0]):
 
@@ -36,8 +39,11 @@ with A = all black pixel
 
 
 def erode_binary_image(input_image, structuring_element):
+    # Create the output image and load it
     output_image = Image.new("RGB", input_image.size, "white")
     image_map = output_image.load()
+    
+    # Look at all pixel in the original Image
     for y in range(0, input_image.size[1]):
         for x in range(0, input_image.size[0]):
 
@@ -50,7 +56,7 @@ def erode_binary_image(input_image, structuring_element):
                 # Check if u and v are inside the image size
                 if 0 <= u < input_image.size[0] and 0 <= v < input_image.size[1]:
 
-                    # If the pixel is white at u,v, break and set overlaps_completely to false
+                    # If the pixel is white at u,v, break and set overlaps_completely to False
                     if input_image.getpixel((u, v))[0] == 255:
                         overlaps_completely = False
                         break
